f293d4f759750be43bb9e1ae659c8102cee96b62
Pulkit Saxena <pulkitsaxena795@gmail.com>
2022-08-20 15:52:54 +0530

    implemented manual mode toggle and chngs for it

--- main.js
+++ main.js
@@ -9,3 +9,55 @@ navBtn.addEventListener("click", () => {
   menuBar.classList.toggle('hidden');
   menuBar.classList.toggle('flex');
 });
+
+
+// Manual dark/light mode toggle 
+
+// Icons
+const moonIcon = document.querySelector(".moon");
+const sunIcon = document.querySelector(".sun");
+
+// Theme vars
+const userTheme = localStorage.getItem("theme");
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+// Icon toggling
+const iconToggle = () => {
+  moonIcon.classList.toggle("display-none");
+  sunIcon.classList.toggle("display-none");
+}
+
+// Initial theme check
+const themeCheck = () => {
+  if((userTheme === 'dark') || (userTheme && systemTheme)){
+    document.documentElement.classList.add("dark");
+    moonIcon.classList.add("display-none");
+    return;
+  }
+  sunIcon.classList.add("display-none");
+}
+
+// Manual Switch
+const themeSwitch = () => {
+  if(document.documentElement.classList.contains("dark")){
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme","light");
+    iconToggle();
+    return;
+  }
+  document.documentElement.classList.add("dark");
+  localStorage.setItem("theme","dark");
+  iconToggle();
+}
+
+// call them and switch the mode
+moonIcon.addEventListener("click",()=>{
+  themeSwitch();
+});
+
+sunIcon.addEventListener("click",()=>{
+  themeSwitch();
+});
+
+// invoke them on inital load
+themeCheck();
